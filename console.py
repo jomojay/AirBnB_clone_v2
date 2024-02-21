edb76a8e6d72adc421f0a8d907f0b73cfa618c2d
maazalside <maazalside@gmail.com>
2024-02-21 08:53:42 +0300

    fix do_all func to take more than two par

--- console.py
+++ console.py
@@ -206,7 +206,8 @@ class HBNBCommand(cmd.Cmd):
             if name not in self.classes.keys():
                 print("** class doesn't exist **")
                 return
-            for key, obj in storage.all(self.classes[name]).items():
+        for key, obj in storage.all().items():
+            if key.split('.')[0] == name:
                 print_list.append(str(obj))
         else:
             for key, obj in storage.all().items():
