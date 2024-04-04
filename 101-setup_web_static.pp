5030cc84a70b4bd2bfc4110d9fc25930a9183430
jomojay <djdrjay001@gmail.com>
2024-04-04 14:24:59 +0100

    fixed puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,13 +8,7 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> exec { '/usr/bin/env service nginx start' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
--> exec { '/usr/bin/env echo "<html>
-  	<head>
-	</head>
-  	<body>
-    		Holberton School
-  	</body>
-  </html>" > /data/web_static/releases/test/index.html' : }
+-> exec { '/usr/bin/env echo "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html' : }
 -> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
 -> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env service nginx restart' : }
