3d11b91ccd65cbacff90a8487c4695126acd0b99
jomojay <djdrjay001@gmail.com>
2024-04-04 14:37:08 +0100

    fixed puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,15 +1,46 @@
 # a puppet version of out setup of nginx for web static
-exec { '/usr/bin/env apt-get -y update' : }
--> exec { '/usr/bin/env apt-get -y install nginx' : }
--> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default' : }
--> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default' : }
--> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default' : }
--> exec { '/usr/bin/env # echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html' : }
--> exec { '/usr/bin/env service nginx start' : }
--> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
--> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
--> exec { '/usr/bin/env echo "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html' : }
--> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
--> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
--> exec { '/usr/bin/env service nginx restart' : }
--> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/' : }
+exec { 'apt-update':
+  command => '/usr/bin/env apt-get -y update',
+}
+
+exec { 'install-nginx':
+  command => '/usr/bin/env apt-get -y install nginx',
+}
+
+exec { 'add-nginx-configuration':
+  command => '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\
+              rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+              add_header X-Served-By \"\$HOSTNAME\";
+              error_page 404 /custom_404.html;" /etc/nginx/sites-available/default',
+  require => Exec['install-nginx'],
+  notify  => Service['nginx'],
+}
+
+exec { 'create-custom-404-page':
+  command => '/usr/bin/env bash -c "echo \"Ceci n\'est pas une page\" > /var/www/html/custom_404.html"',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>",
+}
+
+file { '/data/web_static/current':
+  ensure  => link,
+  target  => '/data/web_static/releases/test/',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "# Your Nginx configuration content here",
+  notify  => Service['nginx'],
+}
+
+exec { 'chown-data-directory':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data/',
+}
