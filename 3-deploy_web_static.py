515e1fdbe9949e98b5f47256066ddc07304546b0
jomojay <djdrjay001@gmail.com>
2024-04-04 05:30:56 -0700

    initial commit for all task

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" a module to package web_static files """
+from fabric.api import local, env, put, run
+from datetime import datetime
+
+
+env.user = 'ubuntu'
+env.hosts = ['100.26.227.200', '52.205.81.70']
+
+
+def do_pack():
+    """
+    Targging project directory into a packages as .tgz
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploy the boxing package tgz file
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """ package && deploy to servers """
+    path = do_pack()
+    if path is None:
+        return False
+    return(do_deploy(path))
